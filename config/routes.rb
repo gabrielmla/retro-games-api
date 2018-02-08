10afd3a24aa6ef32075a0930fc94bd84bf838104
Gabriel Morais <gabmla19@gmail.com>
2018-02-08 13:57:58 -0300

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  get 'game/index'
 
-  get 'game/:id'
-
-  post 'game/create'
-
-  put 'game/:id'
-
-  delete 'game/:id'
-
-  resources :games, only: [:index, :show, :create, :update, :destroy]
+  resources :game, only: [:index, :show, :create, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
